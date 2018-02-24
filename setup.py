cd851d29161d8168e1cee2125f19c95790576322
Jesse Kleve <jesse.kleve@gmail.com>
2018-02-23 18:01:37 -0700

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='map_input',
-    version='0.2.0',
+    version='0.2.1',
     description='Map keyboard & joystick buttons/axes to events',
     license='MIT',
     author='Jesse Kleve',
